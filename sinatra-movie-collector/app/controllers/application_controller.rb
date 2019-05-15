deec0ea2f831d602633e56feaac2be03b9e99586
Ryan Mui <reachryanmui@gmail.com>
2019-05-15 03:55:53 +0000

    building sessions login.html

--- sinatra-movie-collector/app/controllers/application_controller.rb
+++ sinatra-movie-collector/app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    # enable :sessions
-    # set :session_secret, "carcollection"
+    enable :sessions
+    set :session_secret, "authorization_movie_collector"
   end
 
   get '/' do
