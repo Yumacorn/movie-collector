89ff5b40dcdc1dc9c67e136a6e31c48ce4273b4d
Ryan Mui <reachryanmui@gmail.com>
2019-05-31 04:39:41 +0000

    extra validations for user entry

--- sinatra-movie-collector/app/models/user.rb
+++ sinatra-movie-collector/app/models/user.rb
@@ -6,9 +6,14 @@ class User < ActiveRecord::Base
   validates :password, :presence => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
-  validates :zip_code, :presence => true
+  validates :zip_code, :presence => true, numericality: { only_integer: true }
   # validates :first_name, presence: { message: "must be given please" }
   # validates :last_name, presence: { message: "must be given please" }
 
   has_many :movies
+
+  #possible add "acceptance: true" validation for terms of service?
+  #format validation 2.5 validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/,
+  #  message: "only allows letters" }
+  #length 2.7
 end
