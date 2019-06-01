4f750e31ce614907f4945a8b61ec03f88b2a22e3
Ryan Mui <reachryanmui@gmail.com>
2019-06-01 17:48:09 +0000

    rollback db:migrations to version:0

--- sinatra-movie-collector/db/schema.rb
+++ sinatra-movie-collector/db/schema.rb
@@ -10,17 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_031935) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
